32da450e6edd33ac8f81eccde481e9511639cbea
khaledui <khaledui.72@gmail.com>
2026-02-27 03:11:07 +0530

    nots app

--- src/App.jsx
+++ src/App.jsx
@@ -1,89 +1,64 @@
-
-import React from 'react'
-import './index.css'
-import Card from './components/Card'
-import CardProperties from './components/CardProperties';
+import React, { useState } from "react";
+import "./index.css";
 
 const App = () => {
 
-  const cards = [
-  {
-    name: "Amit Sharma",
-    age: 28,
-    address: "Mumbai, India",
-    status: "available"
-  },
-  {
-    name: "Rahul Verma",
-    age: 32,
-    address: "Delhi, India",
-    status: "not available"
-  },
-  {
-    name: "Priya Singh",
-    age: 25,
-    address: "Bangalore, India",
-    status: "available"
-  },
-  {
-    name: "Neha Gupta",
-    age: 30,
-    address: "Pune, India",
-    status: "not available"
-  },
-  {
-    name: "Arjun Mehta",
-    age: 27,
-    address: "Ahmedabad, India",
-    status: "available"
-  },
-  {
-    name: "Sneha Reddy",
-    age: 29,
-    address: "Hyderabad, India",
-    status: "available"
-  },
-  {
-    name: "Vikram Patel",
-    age: 35,
-    address: "Surat, India",
-    status: "not available"
-  },
-  {
-    name: "Anjali Nair",
-    age: 26,
-    address: "Kochi, India",
-    status: "available"
-  },
-  {
-    name: "Rohan Das",
-    age: 31,
-    address: "Kolkata, India",
-    status: "not available"
-  },
-  {
-    name: "Karan Malhotra",
-    age: 33,
-    address: "Chandigarh, India",
-    status: "available"
+  const [title, setTitle] = useState('')
+  const [details, setDetails] = useState('')
+  const [task, setTask]= useState([])
+ 
+  const submitHander = (e) =>{
+    e.preventDefault(e);
+    const copyTask = [...task];
+    copyTask.push({title,details})
+    setTask(copyTask)
+    setTitle('')
+    setDetails('')
   }
-];
+
   return (
+    <div className="h-screen lg:flex bg-black text-white">
+      <form onSubmit={(e)=>{
+        submitHander(e)
+      }} className="flex gap-4 lg:w-1/2 p-10 flex-col items-start">
+        <input
+        value={title}
+        onChange={(e) =>
+          setTitle(e.target.value)
+        }
+          className="px-5 w-full font-medium py-2 border-2 outline-none rounded"
+          type="text"
+          placeholder="Enter Notes Heading"
+        />
+        <textarea
+          className="px-5 w-full h-32  font-medium py-2 border-2 outline-none rounded"
+          placeholder="Write Details"
+           value={details}
+        onChange={(e) =>
+          setDetails(e.target.value)
+        }
+        ></textarea>
+        <button className="bg-white font-medium w-full outline-none text-black px-5 py-2 rounded cursor-pointer">
+          Submit
+        </button>
+      </form>
 
-    <div className='parent'>
-    {/* <Card title="one" discription="this is card first" style={{ backgroundColor: "#FFCDD2" }}
-    imgUrl="https://images.unsplash.com/photo-1770037367722-5444567fcb52?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw0fHx8ZW58MHx8fHx8"  />
-    <Card title="two" discription="this is card second" style={{ backgroundColor: "#b90013" }} 
-    imgUrl="https://images.unsplash.com/photo-1771533680002-c063a96c95a4?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyM3x8fGVufDB8fHx8fA%3D%3D" />
-    <Card title="three" discription="this is card third" style={{ backgroundColor: "#0f9649" }} 
-    imgUrl="https://images.unsplash.com/photo-1769708526202-247e1fe5691f?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzOHx8fGVufDB8fHx8fA%3D%3D"/> */}
-   
-   {cards.map(function(k){
-      return <CardProperties name={k.name} age={k.age} address={k.address} status={k.status} />
-   })}
+      <div className="lg:w-1/2 lg:border-l-2 p-10">
+        <h1 className="text-4xl font-bold">Recent Notes</h1>
+        <div className="flex flex-wrap gap-5 mt-5 h-full overflow-auto">
 
+        {task.map(function(elem, idx){
+          return <div key={idx} className="h-52 w-52 bg-white text-black rounded-2xl p-4">
+            <h1 className="text-xl font-bold text-red-400">{elem.title}</h1>
+            <p className="text-[16px] leading-0 mt-2 font-normal text-gray-400">{elem.details}</p>
+          </div>
+        })}
+
+          
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
